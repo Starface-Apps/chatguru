ba0ee9bbf4ffa3a189fa0f710e1341670e34d4fe
israeloriente <israelnunesoriente@gmail.com>
2024-06-14 00:54:22 -0300

    feat: whatsappField finished

--- cypress/support/planosPrecos.ts
+++ cypress/support/planosPrecos.ts
@@ -1,17 +1,12 @@
 /// <reference types="Cypress" />
 
 Cypress.Commands.add("setNumberPhone", (number: string, delay = true) => {
-  cy.get("#countrynumber2")
-    .click()
-    .type(`${number}`, { delay: delay ? 10 : 0 });
+  cy.get("#countrynumber2").type(`${number}`, { delay: delay ? 30 : 0 });
 });
 
-Cypress.Commands.add(
-  "setContactInfo",
-  (fistName: string, lastName: string, email: string, company: string, delay = true) => {
-    cy.get("#stepsFirstName2").type(fistName, { delay: delay ? 10 : 0 });
-    cy.get("#stepsSobreNome2").type(lastName, { delay: delay ? 10 : 0 });
-    cy.get("#stepsEmail2").type(email, { delay: delay ? 10 : 0 });
-    cy.get("#stepsEmpresa2").type(company, { delay: delay ? 10 : 0 });
-  }
-);
+Cypress.Commands.add("setContactInfo", (fistName, lastName, email, company, delay = true) => {
+  cy.get("#stepsFirstName2").type(fistName, { delay: delay ? 30 : 0 });
+  cy.get("#stepsSobreNome2").type(lastName, { delay: delay ? 30 : 0 });
+  cy.get("#stepsEmail2").type(email, { delay: delay ? 30 : 0 });
+  cy.get("#stepsEmpresa2").type(company, { delay: delay ? 30 : 0 });
+});
