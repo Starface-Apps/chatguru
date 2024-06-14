9aedfa073a2d47294bbb2e17e42d23a5d6ab6155
israeloriente <israelnunesoriente@gmail.com>
2024-06-14 12:38:12 -0300

    feat: changing struture from comands params, adding tests, adding faker

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,9 +1,16 @@
-import "./planosPrecos";
+import "./planosPrecos.command";
 
 // cypress/support/
 declare global {
   namespace Cypress {
     interface Chainable {
+      /**
+       * Set the number phone in the form
+       * @param {string}
+       * @param {boolean} delay - The delay on type()
+       * @example cy.setNumberPhone("83999103042")
+       */
+      setNumberPhone(numberPhone?: numberPhone): Chainable<JQuery<HTMLElement>>;
       /**
        * Set the contact information in the form
        * @param {string} fistName - The first name
@@ -11,22 +18,36 @@ declare global {
        * @param {string} email - The email
        * @param {string} company - The company
        * @param {boolean} delay - The delay on type()
-       * @example cy.setContactInfo("Israel", "Oriente", "israel@gmail.pt", "Orientech")
+       * @example cy.setContactInfo({ fistName: "Israel", lastName: "Oriente", email: "io@gmail.com", company: "IO" })
        */
-      setContactInfo(
-        fistName: string,
-        lastName: string,
-        email: string,
-        company: string,
-        delay?: boolean
-      ): Chainable<JQuery<HTMLElement>>;
+      setContactInfo(setContactInfo?: contactInfo): Chainable<JQuery<HTMLElement>>;
       /**
-       * Set the number phone in the form
-       * @param {string}
+       * Set the user and number phones quantity in the form
+       * @param {number} qtdUser - The quantity of user
+       * @param {boolean} moreOneNumber - If the user want more than one number
        * @param {boolean} delay - The delay on type()
-       * @example cy.setNumberPhone("83999103042")
+       * @example cy.setUserAndQuantity({ qtdUser: 10, moreOneNumber: true })
        */
-      setNumberPhone(numberPhone: string, delay?: boolean): Chainable<JQuery<HTMLElement>>;
+      setUserAndQuantity(userAndQuantity?: userAndQuantity): Chainable<JQuery<HTMLElement>>;
+
+      /**
+       * Fill the form with a number phone and assertions.
+       * @param delay - The delay on type()
+       * @example cy.numberPhoneExample()
+       */
+      numberPhoneExample(delay?: boolean): Chainable<JQuery<HTMLElement>>;
+      /**
+       * Fill the form with contact information and assertions.
+       * @param delay - The delay on type()
+       * @example cy.contactInfoExample()
+       */
+      contactInfoExample(delay?: boolean): Chainable<JQuery<HTMLElement>>;
+      /**
+       * Fill the form with user and number phones quantity and assertions.
+       * @param delay - The delay on type()
+       * @example cy.UserAndQuantityExample()
+       */
+      UserAndQuantityExample(delay?: boolean): Chainable<JQuery<HTMLElement>>;
     }
   }
 }
---
+++ cypress/support/types.command.ts
@@ -0,0 +1,16 @@
+type numberPhone = {
+  numberPhone?: string;
+  delay?: boolean;
+};
+type contactInfo = {
+  fistName?: string;
+  lastName?: string;
+  email?: string;
+  company?: string;
+  delay?: boolean;
+};
+type userAndQuantity = {
+  qtdUser?: number;
+  moreOneNumber?: boolean;
+  delay?: boolean;
+};
